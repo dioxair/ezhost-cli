ca3d3de9838eaa7e92c480cb15b684bee9d52b43
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2023-05-12 17:14:10 -0400

    Setup prompt for service to use

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,50 @@
 import inquirer from "inquirer";
 
 let authKey: String;
+let serviceToUse: String;
 
-let authPrompt = await inquirer.prompt([
+let authPrompt: any = await inquirer.prompt([
   {
     type: "password",
     name: "authKey",
-    message: "Please input your E-Z.host authentication key.",
+    message: "Please input your E-Z.host authentication key:",
     mask: "*",
   },
 ]);
 
 authKey = authPrompt.authKey;
+
+let servicePrompt = await inquirer.prompt([
+  {
+    type: "list",
+    name: "serviceToUse",
+    message: "What do you want to do on E-Z.host?",
+    choices: [
+      "Upload files",
+      "Delete files",
+      "Create URL shortener",
+      "Delete URL shortener",
+      "Create paste",
+      "Edit existing paste",
+      "Delete paste",
+    ],
+  },
+]);
+
+servicePrompt = servicePrompt.serviceToUse;
+
+switch (servicePrompt) {
+  case "Upload files":
+    uploadFile();
+}
+
+async function uploadFile() {
+  let filePrompt = await inquirer.prompt([
+    {
+      type: "input",
+      name: "pathToFile",
+      message: "Please input the path to the file to want to upload:",
+    },
+  ]);
+  console.log(filePrompt.pathToFile);
+}
