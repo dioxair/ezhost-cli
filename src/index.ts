ed0f258d0cc0167fdd54d7ed428adbd8a3cb2edd
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2023-05-12 16:30:49 -0400

    Prompt for e-z.host auth key

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
 import inquirer from "inquirer";
+
+inquirer.prompt([
+  {
+    type: "password",
+    name: "authKey",
+    message: "Please input your E-Z.host authentication key.",
+    mask: "*",
+  },
+]);
